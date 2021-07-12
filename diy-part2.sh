277f693f420d8ca88760d7a63cb56974ea43bc5f
AristotleLai <10828408+AristotleLai@users.noreply.github.com>
2021-07-12 20:29:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,42 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
+
+wget -c -O /tmp/master.zip https://github.com/kenzok8/openwrt-packages/archive/refs/heads/master.zip
+sudo unzip /tmp/master.zip
+rm -rf /tmp/master.zip
+wget -c -O /tmp/master.zip https://github.com/kenzok8/small/archive/refs/heads/master.zip
+sudo unzip /tmp/master.zip
+rm -rf /tmp/master.zip
+
+cp -r /tmp/small-master/trojan-plus/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/small-master/trojan-go/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/small-master/chinadns-ng/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/small-master/brook/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/screenshot/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/naiveproxy/ /home/bensonlai/lede/package/lean/
+
+cp -r /tmp/openwrt-packages-master/AdGuardHome/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/luci-app-adguardhome/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/smartdns/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/luci-app-smartdns/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/luci-app-passwall/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/luci-app-openclash/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/luci-app-smartdns/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/luci-app-clash/ /home/bensonlai/lede/package/lean/
+
+
+
+cp -r /tmp/openwrt-packages-master/luci-theme-argon_new/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/luci-theme-atmaterial/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/luci-theme-opentomato/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/luci-theme-opentomcat/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/luci-theme-opentopd/ /home/bensonlai/lede/package/lean/
+cp -r /tmp/openwrt-packages-master/luci-app-argon-config/ /home/bensonlai/lede/package/lean/
+
+cp -r /tmp/openwrt-packages-master/luci-theme-argon_new/ /home/bensonlai/lede/feeds/luci/themes
+cp -r /tmp/openwrt-packages-master/luci-theme-atmaterial/ /home/bensonlai/lede/feeds/luci/themes
+cp -r /tmp/openwrt-packages-master/luci-theme-opentomato/ /home/bensonlai/lede/feeds/luci/themes
+cp -r /tmp/openwrt-packages-master/luci-theme-opentomcat/ /home/bensonlai/lede/feeds/luci/themes
+cp -r /tmp/openwrt-packages-master/luci-theme-opentopd/ /home/bensonlai/lede/feeds/luci/themes
+cp -r /tmp/openwrt-packages-master/luci-app-argon-config/ /home/bensonlai/lede/feeds/luci/themes
